3a0d9270a0291c138e2fdc121b668cb4907dec07
humphrey <hbutau35@gmail.com>
2023-06-30 19:48:15 +0200

    initial commit

---
+++ content/how-to-win-friends-and-influence-python-developers.rst
@@ -0,0 +1,10 @@
+How to win friends and influence Zimbo developers
+##################################################
+
+:date: 2023-06-29 10:20
+:modified: 2023-06-29 10:20
+:tags: python,django
+:category: community
+:slug: how-to-win-friends-and-influence-zimbabwean-developers
+:authors: Humphrey
+:status: draft
---
+++ pelicanconf.py
@@ -0,0 +1,49 @@
+AUTHOR = "Humphrey"
+SITENAME = "Hamub"
+SITEURL = ""
+
+TITLE = "hamub"
+BIO = "Open Sourcerer who is learning how to run a marathon"
+THEME = "themes/pelican-hyde"
+COLOR_THEME = "08"
+PROFILE_IMAGE = "hamub.jpeg"
+# PROFILE_IMAGE = ""
+STATIC_PATHS = ["images"]
+
+PATH = "content"
+
+TIMEZONE = "Africa/Harare"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+MENUITEMS = (
+        ('about', 'https://hbutau.github.io/about'),
+        )
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("twitter", "htttps://twitter.com/hamub"),
+    ("github", "https://github.com/hbutau"),
+)
+
+TWITTER_USERNAME = "hamub"
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
